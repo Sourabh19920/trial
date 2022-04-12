4baa875e99974ebb291275980ac64a1e659a386b
Sourabh19920 <69756187+Sourabh19920@users.noreply.github.com>
2022-04-12 14:39:22 +0530

    Add files via upload

---
+++ FunWithFlags-master/FunWithFlags/Model/Question.swift
@@ -0,0 +1,27 @@
+//
+//  Question.swift
+//  FunWithFlags
+//
+//  Created by Victor on 2017-12-16.
+//  Copyright © 2017 Victor. All rights reserved.
+//
+
+import Foundation
+
+class Question {
+    let question: String
+    let optionA: String
+    let optionB: String
+    let optionC: String
+    let optionD: String
+    let correctAnswer: Int
+    
+    init(questionText: String, choiceA: String, choiceB: String, choiceC: String, choiceD: String, answer: Int){
+        question = questionText
+        optionA = choiceA
+        optionB = choiceB
+        optionC = choiceC
+        optionD = choiceD
+        correctAnswer = answer
+    }
+}
---
+++ FunWithFlags-master/FunWithFlags/Model/QuestionBank.swift
@@ -0,0 +1,23 @@
+//
+//  QuestionBank.swift
+//  FunWithFlags
+//
+//  Created by Victor on 2017-12-16.
+//  Copyright © 2017 Victor. All rights reserved.
+//
+
+import Foundation
+
+class QuestionBank{
+    var list = [Question]()
+    
+    init() {
+        list.append(Question( questionText: "Entomology is the science that studies", choiceA: "Behavior of human beings", choiceB: "Insects", choiceC: "The origin and history of technical and scientific terms", choiceD: "The formation of rocks", answer: 2))
+        list.append(Question( questionText: "Entomology is the science that studies", choiceA: "Behavior of human beings", choiceB: "Insects", choiceC: "The origin and history of technical and scientific terms", choiceD: "The formation of rocks", answer: 2))
+        list.append(Question( questionText: "Entomology is the science that studies", choiceA: "Behavior of human beings", choiceB: "Insects", choiceC: "The origin and history of technical and scientific terms", choiceD: "The formation of rocks", answer: 2))
+        list.append(Question( questionText: "Entomology is the science that studies", choiceA: "Behavior of human beings", choiceB: "Insects", choiceC: "The origin and history of technical and scientific terms", choiceD: "The formation of rocks", answer: 2))
+        
+        }
+    
+    
+}
